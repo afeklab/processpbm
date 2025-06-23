dfd18e775f8a0d91d06a1df65a0e9ccf1a3090f6
Yuval Bayer <yuval.bayer@weizmann.ac.il>
2025-06-24 00:13:34 +0300

    unify log and results for masliner and normalize - create process script

---
+++ process.py
@@ -0,0 +1,78 @@
+from utils import *
+
+path1 = 'data/488nm_800_80_1-KLF3LC_2-KLF3HC_3-SP1LC_4-SP1HC_5-IRF1_13.6.24_2-5.gpr'
+path2 = 'data/488nm_1000_80_1-KLF3LC_2-KLF3HC_3-SP1LC_4-SP1HC_5-IRF1_13.6.24_2-5.gpr'
+path_fasta = 'data/086902_D_Fasta_20220417.txt'
+f1 = '488 Median'
+f2 = '488 Median'
+path_result = None
+ll = 200
+lh = 40_000
+radius = 7
+
+# Set path_result directory tail as the one of path1 by default
+if path_result == None:
+    path_result = split(path1)[0]
+path_masliner_png = join(path_result, 'masliner.png')
+path_normalize_png = join(path_result, 'normalize.png')
+path_processed_csv = join(path_result, 'processed.csv')
+path_log = join(path_result, 'log.txt')
+path_combinatorial = join(path_result, 'combinatorial.txt')
+
+# Read GPRs
+gpr1 = read_gpr(path1, f1) 
+gpr2 = read_gpr(path2, f2) 
+
+# Assert that the two GPRs are of the same layout and the same flags
+assert np.all(gpr1[COLUMNS] == gpr2[COLUMNS])
+
+# Perform masliner
+y1, y2 = gpr1[f1].values, gpr2[f2].values
+adj, slope, intercept, r, p, se, reg_num = masliner(y1, y2, ll, lh)
+plot_masliner(y1, y2, adj, path_masliner_png)
+
+# Create processed DataFrame with adjusted intensities
+df = gpr2.drop(f2, axis=1)
+df[f'Low - {f1}'] = y1
+df[f'High - {f2}'] = y2
+df['Adj'] = adj
+
+# Perform normalization
+df = normalize(df, path_fasta, 'Adj', radius=radius)
+plot_normalize(df, 'Adj', path_normalize_png)
+df.to_csv(path_processed_csv)
+
+# Create combinatorial file
+df_comb = df[['Sequence', 'Norm']]
+df_comb = df_comb[~ df_comb['Norm'].isna()]
+df_comb.sort_values('Norm', ascending=False, inplace=True)
+df_comb.to_csv(path_combinatorial, header=False, index=False, sep='\t')
+
+# Save result log
+s = ''
+s += '# Input information\n\n'
+s += f'Low-scan GPR path = {path1}\n'
+s += f'High-scan GPR path = {path2}\n'
+s += f'f1 = {f1}\n'
+s += f'f2 = {f2}\n'
+s += f'll = {ll}\n'
+s += f'lh = {lh}\n\n'
+s += f'Fasta path = {path_fasta}\n'
+s += f'Radius = {radius}\n\n'
+s += '## Linear regression information\n\n'
+s += f'Number of total probes = {len(y1)}\n'
+s += f'Number of probes used for regression = {reg_num}\n'
+s += f'Slope = {slope}\n'
+s += f'Intercept = {intercept}\n'
+s += f'Pearson correlation coefficient = {r}\n'
+s += f'p-value = {p}\n'
+s += f'Standrad error of the estimated slope = {se}\n\n'
+s += '# Output information\n\n'
+s += f'Processed path = {path_processed_csv}\n'
+s += f'Combinatorial path = {path_combinatorial}\n'
+s += f'Masliner figure path = {path_masliner_png}\n'
+s += f'Normalize figure path = {path_normalize_png}\n'
+s += f'Log path = {path_log}\n\n'
+
+with open(path_log, 'w') as file:
+    file.write(s)
--- utils.py
+++ utils.py
@@ -36,93 +36,18 @@ def read_fasta(path):
     fasta = fasta.str.extract(r'(?P<ID>.*)\n(?P<Sequence>.*)\n') # Extract ID and sequence using regular expressions (see function documentation for explanation)
     return fasta
 
-def masliner(path1, path2, f1, f2, ll=200, lh=40_000, path_result=None):
-
-    # Set path_result directory tail as the one of path1 by default
-    if path_result == None:
-        path_result = split(path1)[0]
-    path_csv = join(path_result, 'masliner.csv')
-    path_png = join(path_result, 'masliner.png')
-    path_txt = join(path_result, 'masliner.txt')
-
-    # Read GPRs
-    gpr1 = read_gpr(path1, f1) 
-    gpr2 = read_gpr(path2, f2) 
-
-    # Assert that the two GPRs are of the same layout and the same flags
-    assert np.all(gpr1[COLUMNS] == gpr2[COLUMNS])
-
-    # Get fluorescent intensity
-    y1, y2 = gpr1[f1].values, gpr2[f2].values
+def masliner(y1, y2, ll=200, lh=40_000):
 
     # Mask range for linear regression and perform linear regression (PerformStraightRegression in original)
     mask = (y1 >= ll) * (y2 >= ll) * (y1 <= lh) * (y2 <= lh)
+    reg_num = np.sum(mask)
     slope, intercept, r, p, se = linregress(y1[mask], y2[mask])
     linreg_pred = (y1 * slope + intercept + 0.5).astype(int) # Round to integers (PerformAdjustment in original)
     adj = np.where(y2 > lh, linreg_pred, y2)
-    gpr2.drop(f2, axis=1, inplace=True)
-    gpr2[f'Low - {f1}'] = y1
-    gpr2[f'High - {f2}'] = y2
-    gpr2['Adj'] = adj
+    return adj, slope, intercept, r, p, se, reg_num
 
-    # Save adjusted gpr file
-    gpr2.to_csv(path_csv)
+def normalize(df, path_fasta, f='Adj', radius=7, custom_mask=True):
 
-    # Plot result
-    fig, axis = plt.subplots()
-    axis.set_title('Masliner adjusted result')
-    axis.scatter(y1, y2, label='original')
-    axis.scatter(y1, adj, label='adjusted')
-    axis.set_xlabel('Low-scan fluorescent intensity')
-    axis.set_ylabel('High-scan fluorescent intensity')
-    axis.legend()
-    plt.savefig(path_png)
-
-    # Save result log
-    s = ''
-    s += '# Masliner result\n\n'
-    s += '## Input information\n\n'
-    s += f'Low-scan GPR path = {path1}\n'
-    s += f'High-scan GPR path = {path2}\n'
-    s += f'f1 = {f1}\n'
-    s += f'f2 = {f2}\n'
-    s += f'll = {ll}\n'
-    s += f'lh = {lh}\n\n'
-    s += '## Linear regression information\n\n'
-    s += f'Number of total probes = {len(y1)}\n'
-    s += f'Number of probes used for regression = {np.sum(mask)}\n'
-    s += f'Slope = {slope}\n'
-    s += f'Intercept = {intercept}\n'
-    s += f'Pearson correlation coefficient = {r}\n'
-    s += f'p-value = {p}\n'
-    s += f'Standrad error of the estimated slope = {se}\n\n'
-    s += '## Output information\n\n'
-    s += f'Masliner result path = {path_csv}\n'
-    s += f'Masliner figure path = {path_png}\n'
-    s += f'Masliner log path = {path_txt}\n\n'
-    with open(path_txt, 'w') as file:
-        file.write(s)
-
-# Custom formatter: convert to thousands with 'k'
-def k_formatter(x, pos):
-    return f'{int(x/1000)}k'
-
-def normalize(path, path_fasta, f='Adj', radius=7, custom_mask=True, path_result=None):
-
-    # Set path_result directory tail as the one of path1 by default
-    if path_result == None:
-        path_result = split(path1)[0]
-    path_csv = join(path_result, 'normalize.csv')
-    path_comb = join(path_result, 'combinatorial.txt')
-    path_png = join(path_result, 'normalize.png')
-    path_txt = join(path_result, 'normalize.txt')
-
-    # Get data
-    if path[-4:] == '.gpr':
-        df = read_gpr(path, f)
-    else:
-        df = pd.read_csv(path, index_col=0)
-    
     column = df['Column'].values
     row = df['Row'].values
     y = df[f].values
@@ -175,20 +100,13 @@ def normalize(path, path_fasta, f='Adj', radius=7, custom_mask=True, path_result
     left_adj = np.where(con, col_max - 2 * radius, left_adj)
     right_adj = np.where(con, col_max, right_adj)
 
-    df['Top'] = top_adj
-    df['Bottom'] = bottom_adj
-    df['Left'] = left_adj
-    df['Right'] = right_adj
-
     # Normalize
     median_global = np.median(y)
     median_local = np.empty(len(df))
     size = np.empty(len(df))
-    '''
-    print('Running window')
-    for i in tqdm(range(len(df))):
-    
-        t, b, l, r = df[['Top', 'Bottom', 'Left', 'Right']].iloc[i].values
+
+    for i, t, b, l, r in zip(range(len(df)), top_adj, bottom_adj, left_adj, right_adj):# Note that running an index, take row from the dataframe, convert it to nu,py values at each iteration is slow
+        
         y_window = y_arr[t - 1: b, l - 1: r].flatten()
         mask_window = mask_arr[t - 1: b, l - 1: r].flatten()
         median_local[i] = np.median(y_window[mask_window])
@@ -202,55 +120,55 @@ def normalize(path, path_fasta, f='Adj', radius=7, custom_mask=True, path_result
     norm = np.where((~ mask_flags) + (~ mask_seq), np.nan, norm)
 
     # Save result
+    df['Top'] = top_adj
+    df['Bottom'] = bottom_adj
+    df['Left'] = left_adj
+    df['Right'] = right_adj
     df['Local window median'] = median_local
     df['Window size'] = size
     df['Norm'] = norm
-    df.to_csv(path_csv)
-    '''
-    df = pd.read_csv(path_csv)
 
-    df_comb = df[['Sequence', 'Norm']]
-    df_comb = df_comb[~ df_comb['Norm'].isna()]
-    df_comb.sort_values('Norm', ascending=False, inplace=True)
-    df_comb.to_csv(path_comb, header=False, index=False, sep='\t')
+    return df
+def plot_masliner(y1, y2, adj, path):
+    
+    fig, axis = plt.subplots()
+    axis.set_title('Masliner adjusted result')
+    axis.scatter(y1, y2, label='original')
+    axis.scatter(y1, adj, label='adjusted')
+    axis.set_xlabel('Low-scan fluorescent intensity')
+    axis.set_ylabel('High-scan fluorescent intensity')
+    axis.legend()
+    plt.savefig(path)
 
+
+def plot_normalize(df, f, path):
+    
+    column = df['Column'].values
+    row = df['Row'].values
+    col_max, row_max = np.max(column), np.max(row)
+
+    y_arr = df[f].values.reshape(row_max, col_max)
+    norm_arr = df['Norm'].values.reshape(row_max, col_max)
+
+    k_formatter = lambda x, pos : f'{int(x/1000)}k' # Custom formatter: convert to thousands with 'k'
+    
     # Plots result
     fig, axes = plt.subplots(1, 3, figsize=(12, 4))
-    arrays = [y_arr, df['Norm'].values.reshape(row_max, col_max), mask_arr]
-    titles = [f, 'Norm', 'Norm mask (binary)']
+    arrays = [y_arr, norm_arr]
+    titles = [f, 'Normalized']
 
-    for i, ax, arr, title in zip(range(3), axes, arrays, titles):
+    for ax, arr, title in zip(axes, arrays, titles):
         im = ax.matshow(arr)
         ax.set_title(title)
 
-        if i < 2:
-            
-            # Create a divider for each axis
-            divider = make_axes_locatable(ax)
-            cax = divider.append_axes("right", size="5%", pad=0.05)
-            cbar = fig.colorbar(im, cax=cax)
-            cbar.ax.yaxis.set_major_formatter(FuncFormatter(k_formatter))
+        # Create a divider for each axis
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        cbar = fig.colorbar(im, cax=cax)
+        cbar.ax.yaxis.set_major_formatter(FuncFormatter(k_formatter))
 
     plt.tight_layout()
-    plt.savefig(path_png)
+    plt.savefig(path)
 
-    # Save result log
-    s = ''
-    s += '# Normalize result\n\n'
-    s += '## Input information\n\n'
-    s += f'Data path = {path}\n'
-    s += f'Fasta path = {path_fasta}\n'
-    s += f'f = {f}\n'
-    s += f'Radius = {radius}\n\n'
-    s += '## Output information\n\n'
-    s += f'Normalize result path = {path_csv}\n'
-    s += f'Combinatorial path = {path_comb}\n'
-    s += f'Normalize figure path = {path_png}\n'
-    s += f'Normalize log path = {path_txt}\n\n'
-    with open(path_txt, 'w') as file:
-        file.write(s)
-    
-path1 = 'data/488nm_800_80_1-KLF3LC_2-KLF3HC_3-SP1LC_4-SP1HC_5-IRF1_13.6.24_2-5.gpr'
-path2 = 'data/488nm_1000_80_1-KLF3LC_2-KLF3HC_3-SP1LC_4-SP1HC_5-IRF1_13.6.24_2-5.gpr'
-# masliner(path1, path2, 'F488 Median', 'F488 Median')
-normalize('data/masliner.csv', 'data/086902_D_Fasta_20220417.txt')
+
+
